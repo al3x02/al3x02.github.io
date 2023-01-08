d2ed987a77d83cae60899dd71720221d3ad4202a
al3x02 <alexander10.issal@gmail.com>
2023-01-08 14:59:59 +0100

    Invisible ingidients by default
    Added a "select" element in HTML and styled it in CSS code, ingredients will be invisible by default and pop up when user selects "add ingredient". Added a remove button to be able to delete the ingredient

--- calculator.js
+++ calculator.js
@@ -21,6 +21,74 @@ const ingredients = {
   }
 };
 
+
+function createIngredientElement(ingredientName) {
+  const ingredientElement = document.createElement('div');
+  ingredientElement.id = ingredientName;
+  ingredientElement.style.display = 'none';
+
+  const label = document.createElement('label');
+  label.setAttribute('for', ingredientName);
+  label.textContent = `${ingredientName} (mg):`;
+
+  const input = document.createElement('input');
+  input.type = 'number';
+  input.id = ingredientName;
+
+  ingredientElement.appendChild(label);
+  ingredientElement.appendChild(input);
+
+  return ingredientElement;
+}
+
+
+
+
+
+
+const calculatorForm = document.getElementById('calculator-form');
+const productNameInput = document.getElementById('product-name');
+const betaAlanineInput = document.getElementById('beta-alanine');
+const caffeineInput = document.getElementById('caffeine');
+const creatineInput = document.getElementById('creatine');
+const citrullineInput = document.getElementById('citrulline');
+
+
+
+
+
+
+// Define the toggleIngredient function
+function toggleIngredient(ingredientId) {
+  var ingredientElement = document.getElementById(ingredientId);
+  if (ingredientElement.style.display === "none") {
+    ingredientElement.style.display = "block";
+  } else {
+    ingredientElement.style.display = "none";
+  }
+}
+
+// Add a click event listener to the add-ingredient-button
+document.getElementById('add-ingredient-button').addEventListener('click', function() {
+  // Get the selected ingredient id
+  var ingredientId = document.getElementById('ingredients-select').value;
+  // Toggle the visibility of the corresponding ingredient input
+  toggleIngredient(ingredientId);
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const addIngredientButton = document.getElementById('add-ingredient-button');
+  const ingredientsSelect = document.getElementById('ingredients-select');
+
+  addIngredientButton.addEventListener('click', () => {
+    const ingredient = ingredientsSelect.value;
+    document.getElementById(ingredient).style.display = 'block';
+  });
+});
+
+
+
 function calculateIngredientScore(ingredient) {
   if (ingredient.dosage < ingredient.minDosage) {
     return 0;
@@ -68,14 +136,28 @@ function displayScores(productName, totalScore, energyScore, strengthScore, endu
 }
 
 
+document.getElementById('add-ingredient-button').addEventListener('click', function() {
+  // Get the selected ingredient
+  const ingredient = document.getElementById('ingredients-select').value;
+
+  // Show the ingredient input field
+  document.getElementById(ingredient).style.display = 'block';
+
+  // Check if a "Delete Ingredient" button already exists for this ingredient
+  if (!document.getElementById(`delete-${ingredient}-button`)) {
+    // If a "Delete Ingredient" button does not already exist, create one
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = 'Delete Ingredient';
+    deleteButton.id = `delete-${ingredient}-button`;
+    deleteButton.addEventListener('click', function() {
+      // When the "Delete Ingredient" button is clicked, hide the corresponding ingredient input field
+      document.getElementById(ingredient).style.display = 'none';
+    });
+    document.getElementById(ingredient).appendChild(deleteButton);
+  }
+});
 
 
-const calculatorForm = document.getElementById('calculator-form');
-const productNameInput = document.getElementById('product-name');
-const betaAlanineInput = document.getElementById('beta-alanine');
-const caffeineInput = document.getElementById('caffeine');
-const creatineInput = document.getElementById('creatine');
-const citrullineInput = document.getElementById('citrulline');
 
 
 
@@ -95,3 +177,4 @@ calculatorForm.addEventListener('submit', (event) => {
 
   displayScores(productName, totalScore, energyScore, strengthScore, enduranceScore);
 });
+
